3d967ba24d5d4f43913b7156d8e7358b614d3bbd
Ewa Czech <eaprojekty@gmail.com>
2018-03-27 00:20:41 +0200

    creates and instantiate Enemy and Player objects. Creates prototype.update functions for both objects with rules on how to move around the playground

--- js/app.js
+++ js/app.js
@@ -1,19 +1,30 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+/*
+Enemy class function is created. It takes 3 paramaters: x for enemy's position
+in vertical location (alongside x axis), y for for enemy's location in horizontal
+location (alongside y axis) and speed.
+*/
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+var Enemy = function(x, y, speed) {
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+  this.sprite = 'images/enemy-bug.png';
+  this.radius = 35;
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+/* Update the enemy's position. There are 3 Enemies in different starting location.
+Once Enemy moves out off the screen, its horizontal position is set back to 0, its
+staring position is chosen randomly, so is its speed, which is always more than 100.
+*/
+
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  var enemyLocation = [55, 138, 220];
+  this.x = this.x + (this.speed * dt);
+    if (this.x > 475) {
+      this.x = 0;
+      this.y = enemyLocation[Math.floor((Math.random()) * 3)];
+      this.speed = Math.floor(Math.random() * 200)+100;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,16 +32,42 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+/*
+Player class function is created. It takes 2 paramaters: x for Players's position
+in vertical location (alongside x axis), y for for Players's location in horizontal
+location (alongside y axis)
+*/
 
+var Player = function(x, y) {
+  this.x = x;
+  this.y = y;
+  this.img = 'images/char-boy.png';
+  this.radius = 35;
+}
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Updates Player's position once it reaches the water and increase its score by one.
 
+Player.prototype.update = function(dt) {
+  if(this.y === -25) {
+    this.y = 400;
+    resultBoard.score++;
+    document.getElementById('score').innerHTML = resultBoard.score;
+  }
+};
 
+/* Enemy and Player objects are instantiate. All Enemy objects are placed in
+an array called allEnemies, while Player object is in a variable called player.
+*/
+
+var allEnemies = [];
+var enemyLocation = [55, 138, 220];
+
+enemyLocation.forEach((loc) => {
+  enemy = new Enemy(0, loc, Math.floor((Math.random()) * 200) + 100);
+  allEnemies.push(enemy);
+});
+
+var player = new Player(200, 400);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
