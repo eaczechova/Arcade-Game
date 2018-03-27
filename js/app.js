29d6e232b78ea64f0c4900481dd884d4e42b7841
Ewa Czech <eaprojekty@gmail.com>
2018-03-27 13:01:05 +0200

    add Player handleInput function

--- js/app.js
+++ js/app.js
@@ -62,6 +62,22 @@ Player.prototype.render = function() {
   drawCircle(this.x+50, this.y+100, this.radius, "green");
 }
 
+/* Function checkes for key type and moves Player from it current location. The second
+condition prevents Player to leave the playground.
+*/
+
+Player.prototype.handleInput = function(key) {
+  if (key === 'left' && this.x > 0) {
+    this.x = this.x - 100;
+  } else if (key === 'up' && this.y > 0) {
+    this.y = this.y - 85;
+  } else if (key === 'right' && this.x < 400) {
+    this.x = this.x + 100;
+  } else if (key === 'down' && this.y < 400) {
+    this.y = this.y + 85;
+  }
+}
+
 /* Enemy and Player objects are instantiate. All Enemy objects are placed in
 an array called allEnemies, while Player object is in a variable called player.
 */
@@ -76,17 +92,17 @@ enemyLocation.forEach((loc) => {
 
 var player = new Player(200, 400);
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+//This listens for key presses and sends the keys to your Player.handleInput() method.
 
-    player.handleInput(allowedKeys[e.keyCode]);
+document.addEventListener('keyup', function(e) {
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
+
+  player.handleInput(allowedKeys[e.keyCode]);
 });
 
 /* Add helper function drawCircle() that draw circle around Player and Enemy objects
